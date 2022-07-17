72510bcb822b04d4f2dcf8c9041b8c2232980091
MuntjeerAli <muntjeersayyad@gmail.com>
2022-07-17 21:38:31 +0530

    Tried changing await async

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,16 +7,15 @@ const Weather = () => {
 
     const [city, setCity] = useState(null);
     const [search, setSearch] = useState("Mumbai");
-
     useEffect( () =>{
-       const fetchApi = async () => {
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=a60ccfc1be0e48cdfc44110da0ae2ef1`;
-        const response = await fetch(url);
-        const resJson = response.json();
-        setCity(resJson.main);
-       };
-        fetchApi();
-    }, [search] )
+        const fetchApi = async () => {
+         const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=a60ccfc1be0e48cdfc44110da0ae2ef1`;
+         const response = await fetch(url);
+         const resJson = response.json();
+         setCity(resJson.main);
+        };
+         fetchApi();
+     },[search] );
     return(
         <>
         <div className="content">
@@ -30,24 +29,21 @@ const Weather = () => {
             {!city ? (
                 <p className="msg"> No Data Found</p>
             ) : (
-                <div>
-                     <div className="info">
+            <div>
+                <div className="info">
                     <h1 className="city">
-                    <FaStreetView/> {search}
+                        <FaStreetView/> {search}
                     </h1>
                     <h1 className="weather">
-                        {city.temp}
+                        {city.main.temp}
                     </h1>
                     <h3 className="min-max">
-                        Min: 21°C | Max: 25°C
+                        {city.temp_max} | Max: {city.temp_min}C
                     </h3>
                     </div>
                     <div></div>
-                </div>
-               
-            )
-            }
-            
+            </div>
+            )}
         </div>
             
         </>
